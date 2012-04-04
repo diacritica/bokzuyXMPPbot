b51762f305fd3750c5bf4d813cbee86c1bd7d7a6
Pablo Ruiz Múzquiz <diacritica@elenya.nett>
2012-04-04 17:55:26 +0200

    yet another dummy edit for master branch

--- bokzuy_bot.py
+++ bokzuy_bot.py
@@ -174,6 +174,6 @@ if __name__ == '__main__':
 #        xmpp.process(block=True)
         
         xmpp.process(threaded=False)
-        print("Done")
+        print("Done!")
     else:
         print("Unable to connect.")
